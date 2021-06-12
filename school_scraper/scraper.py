f51c6c23efc56a2bda9d94350bdfbddc0bb92dca
Brandon Sloane <brandon.sloane@nyu.edu>
2021-06-11 21:31:30 -0400

    adding wayback functionality

--- school_scraper/scraper.py
+++ school_scraper/scraper.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import urllib.request
+import requests
+from urllib.request import urlopen
 import re
 import hashlib
 import os
@@ -11,20 +13,25 @@ import threading
 import tldextract
 import subprocess
 import sys
+import waybackpy
+from waybackpy import Url
 
 
 subprocess.call(['mkdir', '-p', 'raw-data'])
 
+year = datetime.datetime.now().year
 
+# MAX_DEPTH = 8
 MAX_DEPTH = 8
 
 
 # Data from https://k12cybersecure.com/2019-year-in-review/
-BASE_PATH = 'https://hdanny.org/static/private_data/k12/'
+# BASE_PATH = 'https://hdanny.org/static/private_data/k12/'
+BASE_PATH = '/home/kali/Documents/PhD/'
 
 # A dataframe of "School District Name" and "Website"
-raw_district_df = pd.read_excel(BASE_PATH + 'SchoolDistrictswIncidents.xlsx').fillna('')
-
+# raw_district_df = pd.read_excel(BASE_PATH + 'SchoolDistrictswIncidents.xlsx').fillna('')
+raw_district_df = pd.read_excel(BASE_PATH + 'SchoolDistrictswIncidents-trim.xlsx').fillna('')
 
 lock = threading.Lock()
 
@@ -34,20 +41,26 @@ def main():
     try:
         thread_count = int(sys.argv[1])
     except Exception:
-        thread_count = 30
+        thread_count = 50
 
     scrape_queue = []
 
-    visited_url = set()
+    #visited_url = set()
+    visited_url = []
 
     # Pre-populate the queue
     for (_, row) in raw_district_df.iterrows():
-        scrape_queue += [{
-            'base_school_name': row['School District Name'],
-            'base_school_website': row['Website'],
-            'visit_url': row['Website'],
-            'depth': 0
-        }]
+        for year in range(2019, 2022):
+            scrape_queue += [{
+                'base_school_name': row['School District Name'],
+                'base_school_website': row['Website'],
+                'visit_url': row['Website'],
+                'depth': 0,
+                'year': year
+            }]
+
+    #print(f'scrape queue:', scrape_queue)
+	
 
     # Resume from previous queue
     try:
@@ -63,7 +76,7 @@ def main():
     except IOError:
         pass
 
-    print('Queue length:', len(scrape_queue))
+    #print('Queue length:', len(scrape_queue))
 
     # Start multiple threads
     thread_list = []
@@ -90,10 +103,13 @@ def process_queue(scrape_queue, visited_url):
                 return
             info = scrape_queue.pop(random.randint(0, len(scrape_queue) - 1))
             queue_length = len(scrape_queue)
+            urlinfo = (info['visit_url'],info['year'])
+            #print(f"processing url info: {urlinfo}")
 
-            # Skip if we have scraped this URL before
-            if info['visit_url'] in visited_url:
-                continue
+        # Skip if we have scraped this URL before
+        if (info['visit_url'],info['year']) in visited_url:
+            #print('visited already')
+            continue
         
         # Stop processing beyond Depth
         if info['depth'] >= MAX_DEPTH:
@@ -103,10 +119,15 @@ def process_queue(scrape_queue, visited_url):
         if info['visit_url'].endswith('.pdf'):
             continue
 
+        # Ignore Telephone numbers
+        if info['visit_url'].startswith('Tel'):
+            continue
+
         # Parse the page
         try:
-            html = get_html(info['visit_url'], queue_length)
+            html = get_html(info['visit_url'], queue_length, info['year'])
         except Exception:
+            #print(f"get html failed: {info['visit_url']}")
             continue
 
         try:
@@ -118,15 +139,34 @@ def process_queue(scrape_queue, visited_url):
         info['title'] = page['title']
         info['timestamp'] = str(datetime.datetime.now())
         with lock:
-            visited_url.add(info['visit_url'])
+            #changed visited_url to a list of tuples
+            visited_url.append((
+                info['visit_url'],
+                info['year']
+                ))
+            #print(f"visited url list: {visited_url}")
+            #visited_url.add(info['visit_url'])
             with open('output.json', 'a') as fp:
                 print(json.dumps(info), file=fp)
 
+
         # Explore more links only if the current page shares the same domain as the district's
         district_domain = tldextract.extract(info['base_school_website']).registered_domain
-        visit_domain = tldextract.extract(info['visit_url']).registered_domain
+
+        if 'web.archive.org' in info['visit_url']:
+            s = info['visit_url']
+            s2 = s.split('//')                
+            try:
+                visit_domain = tldextract.extract(s2[2]).registered_domain
+            except Exception:
+                visit_domain = tldextract.extract(info['visit_url']).registered_domain
+        else:
+            visit_domain = tldextract.extract(info['visit_url']).registered_domain
+        
         if visit_domain != district_domain:
+            print(f'visit_domain: {visit_domain} not equal to district_domain:{district_domain}')
             continue
+
 
         # Stop exploring if reaching max depth
         if info['depth'] + 1 >= MAX_DEPTH:
@@ -136,13 +176,17 @@ def process_queue(scrape_queue, visited_url):
         with lock:
             with open('queue.json', 'a') as fp:
                 for link in page['links']:          
-                    if link in visited_url:
+                    #changed lookup to a tuple
+                    #print(f'link: ', link)
+                    if (link,info['year']) in visited_url:
                         continue  
                     q_element = {
                         'base_school_name': info['base_school_name'],
                         'base_school_website': info['base_school_website'],
                         'visit_url': link,
-                        'depth': info['depth'] + 1
+                        'depth': info['depth'] + 1,
+                        #adding a year frame to the queue
+                        'year': info['year']
                     }
                     scrape_queue.append(q_element)
                     print(json.dumps(q_element), file=fp)
@@ -150,6 +194,8 @@ def process_queue(scrape_queue, visited_url):
 
 def parse_page(html):
     """Returns a dictionary representation of an HTML page."""
+    #debugging the parser
+    #print(f'html parsing:', html)
 
     soup = BeautifulSoup(html, features="html.parser")
 
@@ -166,26 +212,35 @@ def parse_page(html):
     for a_tag in soup.findAll('a', attrs={'href': re.compile("^https://")}):
         links.add(a_tag.get('href'))   
 
+    #print(f'links: ', links)
     return {
         'title': title,
         'links': links
     }
 
 
-def get_html(url, queue_length):
+def get_html(url, queue_length, year):
     """
     Obtains HTML from URL. If already visited, reads from disk.
 
     """
+    if year != '2021':
+        #wayback stuff
+        user_agent = 'User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
+        wayback = Url(url, user_agent)
+        archive_version = wayback.near(year=year)
+        url = archive_version.archive_url
+        
     cached_path = os.path.join('raw-data', hashlib.sha256(url.encode('utf-8')).hexdigest())
 
     # Read from cache
     try:
         with open(cached_path) as fp:
-            print(f'{queue_length}: Cached URL:', url)
+            #print(f'{queue_length}: Cached URL:', archive_url)
             return fp.read()
     except IOError:
         pass
+
 
     # Add headers
     req = urllib.request.Request(url)
@@ -200,10 +255,10 @@ def get_html(url, queue_length):
     with open(cached_path, 'w') as fp:
         fp.write(html.decode('utf-8'))
 
-    print(f'{queue_length}: Fetched URL:', url)
+    # print(f'{queue_length}: Fetched URL: {url}  year:{year}')
+    #print(f'{queue_length}: Fetched URL:', url)
 
     return html
-
 
 
 if __name__ == '__main__':
